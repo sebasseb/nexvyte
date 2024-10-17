edb51319e68f55b99add05f6fbf83d137d342805
Sebastian <sebastian.escobar.dev@gmail.com>
2024-10-16 21:55:32 -0300

    Crear boton + css

---
+++ nexvyte-project/src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+function Button() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Button
---
+++ nexvyte-project/src/components/TextInput/TextInput.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+
+
+function TextInput() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default TextInput
