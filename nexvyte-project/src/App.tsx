3000dacccd69891cd8f4227e79e6983b0756a918
Camilo Dev <kmilo.developer@gmail.com>
2024-10-16 23:13:25 -0300

    pruebas git

--- nexvyte-project/src/App.tsx
+++ nexvyte-project/src/App.tsx
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import RouteList from "./router/routes";
-
-
+import { Footer } from 'antd/es/layout/layout';
 
 function App() {
-
-
-
   return (
-    <Router>
-      <Routes>
-        {Object.values(RouteList).map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-    </Router>
+    <div className="min-h-screen flex flex-col">
+      <Router>
+        <div className="flex-grow">
+          <Routes>
+            {Object.values(RouteList).map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </div>
+      </Router>
+      <Footer className="bg-gray-800 text-white p-4 text-center">
+        Footer Content
+      </Footer>
+    </div>
   );
 }
 
